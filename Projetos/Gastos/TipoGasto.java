0c161c1d2fc970f2b5857830f9e7809e1c01e9ef
Pedro H K Goncalves <108807271+PedroKons@users.noreply.github.com>
2023-05-08 10:53:51 -0300

    Create TipoGasto.java

---
+++ Projetos/Gastos/TipoGasto.java
@@ -0,0 +1,14 @@
+package meupacote;
+
+public class TipoGasto {
+    private String descricao;
+
+    public String getDescricao(){
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+}
